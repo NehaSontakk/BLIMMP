986bae6b89a982b426b2438a0bd9b7d2d9953a13
Neha Sontakke <nsontakke004@gmail.com>
2025-06-18 12:19:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="meta-dawg",
+  version="0.1.0",
+  author="Neha Sontakke",
+  license="MIT",
+  packages=find_packages(),
+  install_requires=[
+    "pandas>=1.5",
+    "numpy>=1.23",
+    # etc.
+  ],
+  entry_points={
+    "console_scripts": [
+      "meta-dawg=meta_dawg.module_detection:main",
+    ],
+  },
+)
 
